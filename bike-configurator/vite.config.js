4dd7836a3fdd83d831ee61c67a5614afd7631a29
Reshmi Kumari Gupta <reshmigupta2014@gmail.com>
2026-06-19 15:37:34 +0530

    Update GitHub Pages deployment configuration

--- bike-configurator/vite.config.js
+++ bike-configurator/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/mini-project/",
+  base: "/Bicycle-builder/",
   plugins: [react()],
   resolve: {
     alias: {
